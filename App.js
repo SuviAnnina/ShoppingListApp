0618f179ff2cb2c640046b847ace49855ecbffde
SuviAnnina <suvi.varjoranta@gmail.com>
2024-01-30 21:18:06 +0200

    Add shoppinglistapp

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  console.log("item state:" + item);
+  console.log(items);
+
+  const handleChange = () => {
+    setItems([...items, item]);
+    setItem("");
+  }
+  console.log("item state:" + item);
+  console.log(items);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <TextInput
+        style={styles.textInput}
+        value={item}
+        onChangeText={text => setItem(text)}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Add"
+          onPress={() => { handleChange() }}
+        />
+        <Button
+          title="Clear list"
+          onPress={() => { setItems([""]) }}
+        />
+
+      </View>
+
+      <View style={styles.shoppinglist}>
+        <Text style={{ color: '#9e3a08', fontSize: 20 }}>Shopping list</Text>
+        <FlatList
+          data={items}
+          renderItem={({ item }) =>
+            <View style={styles.shoppingListItem}>
+              <Text>{item}</Text>
+            </View>}
+        />
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +61,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 150,
   },
+
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 2,
+    width: 250,
+  },
+
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+
+  shoppinglist: {
+    flex: 15,
+    width: 330,
+    alignItems: 'center',
+    backgroundColor: '#f7c5c1',
+  },
+
+  shoppingListItem: {
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
